ec59032e3de1ec9aaec0c813c52ef6f44f389800
Jesus Cidoncha <jesus.cidoncha.berlanga@gmail.com>
2024-01-16 15:02:19 +0100

    changed server app.js

--- server/app.js
+++ server/app.js
@@ -2,11 +2,11 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
-const cohorts = require("./cohorts.json");
-const students = require("./students.json");
+
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
-// ...
+const cohorts = require("./cohorts.json");
+const students = require("./students.json");
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
@@ -22,14 +22,13 @@ app.use(cookieParser());
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
-// ...
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 app.get("/api/cohorts", (req, res) => {
   res.json(cohorts);
 });
-app.get("/api/students", (req, res) => {
+app.get("/api/git", (req, res) => {
   res.json(students);
 });
 // START SERVER
