42b66644639e160b6bc084cd3aed801125ff335b
Catherine Fournier <fourniercath@hotmail.com>
2024-01-17 14:59:56 +0100

    update Cohort model

--- server/models/Cohort.model.js
+++ server/models/Cohort.model.js
@@ -5,16 +5,17 @@ const Schema = mongoose.Schema;
 // CREATE SCHEMA
 // Schema - describes and enforces the structure of the documents
 const cohortSchema = new Schema({
-  inProgress: Boolean,
-  cohortSlug: String,
-  cohortName: String,
-  program: String,
-  campus: String,
-  startDate: String,
-  endDate: String,
-  programManager: String,
-  leadTeacher: String,
-  totalHours: Number,
+  cohortSlug: {type:String, required:true},
+  cohortName: {type:String, required:true},
+  program: {type:String, enum:["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"]},
+  format: {type:String, enum:["Full Time", "Part Time"]},
+  campus: {type:String, enum:["Madrid", "Barcelona", "Miami", "Paris", "Berlin", "Amsterdam", "Lisbon", "Remote"]},
+  startDate: {type:Date, default:Date.now},
+  endDate: Date,
+  inProgress: {type:Boolean, default:false},
+  programManager: {type:String, required:true},
+  leadTeacher: {type:String, required:true},
+  totalHours: {type:Number, default:360},
 });
 
 // CREATE MODEL
