21ce187b28d3f72669fdf762a9e2d715e5215031
Jesus Cidoncha <jesus.cidoncha.berlanga@gmail.com>
2024-01-18 15:20:49 +0100

    Error hanfling

--- server/app.js
+++ server/app.js
@@ -1,43 +1,52 @@
-const cors = require('cors');
-const express = require('express');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const mongoose = require('mongoose');
+const cors = require("cors");
+const express = require("express");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
 const PORT = 5005;
 
 //STATIC DATA
-const studentsRoutes = require('./routes/students-route.js');
-const cohortsRoutes = require('./routes/cohort-routes.js');
+const studentsRoutes = require("./routes/students-route.js");
+const cohortsRoutes = require("./routes/cohort-routes.js");
 
 const app = express();
 
 app.use(
-    cors({
-        origin: ['http://localhost:5005'],
-    })
+  cors({
+    origin: ["http://localhost:5005"],
+  })
 );
 app.use(express.json());
-app.use(morgan('dev'));
-app.use(express.static('public'));
+app.use(morgan("dev"));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 //DOCS ROUTES
-app.get('/docs', (req, res) => {
-    res.sendFile(__dirname + '/views/docs.html');
+app.get("/docs", (req, res) => {
+  res.sendFile(__dirname + "/views/docs.html");
 });
 
-app.use('/api/students', studentsRoutes);
-app.use('/api/cohorts', cohortsRoutes);
+app.use("/api/students", studentsRoutes);
+app.use("/api/cohorts", cohortsRoutes);
 
 mongoose
-    .connect('mongodb://127.0.0.1:27017/cohort-tools-api', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((error) => console.error('Error connecting to MongoDB', error));
+  .connect("mongodb://127.0.0.1:27017/cohort-tools-api", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.error("Error connecting to MongoDB", error));
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
+const {
+  errorHandler,
+  notFoundHandler,
+} = require("./middleware/error-handling");
+
+// Set up custom error handling middleware:
+
+app.use(notFoundHandler);
+app.use(errorHandler);
--- server/routes/cohort-routes.js
+++ server/routes/cohort-routes.js
@@ -1,74 +1,57 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Cohort = require('../models/Cohort.model');
+const Cohort = require("../models/Cohort.model");
 
-router.get('/', async (req, res) => {
-    try {
-        const cohorts = await Cohort.find();
-        res.json(cohorts);
-    } catch (error) {
-        res.status(500).json({
-            error,
-            message: 'Something went wrong on the server',
-        });
-    }
+router.get("/", async (req, res) => {
+  try {
+    const cohorts = await Cohort.find();
+    res.json(cohorts);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post('/', async (req, res) => {
-    try {
-        const newCohort = await Cohort.create(req.body);
-        res.status(201).json(newCohort);
-    } catch (error) {
-        res.status(500).json({
-            error,
-            message: 'Something went wrong on the server',
-        });
-    }
+router.post("/", async (req, res) => {
+  try {
+    const newCohort = await Cohort.create(req.body);
+    res.status(201).json(newCohort);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get('/cohort/:cohortId', async (req, res) => {
-    try {
-        console.log('req.params.cohortId)', req.params.cohortId);
-        const oneCohort = await Cohort.findById(req.params.cohortId);
-        res.json(oneCohort);
-    } catch (error) {
-        res.status(500).json({
-            error,
-            message: 'Something went wrong on the server',
-        });
-    }
+router.get("/cohort/:cohortId", async (req, res) => {
+  try {
+    console.log("req.params.cohortId)", req.params.cohortId);
+    const oneCohort = await Cohort.findById(req.params.cohortId);
+    res.json(oneCohort);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.put('/cohort/:cohortId', async (req, res) => {
-    try {
-        const updatedCohort = await Cohort.findByIdAndUpdate(
-            req.params.cohortId,
-            req.body,
-            { new: true }
-        );
-        res.status(202).json(updatedCohort);
-    } catch (error) {
-        res.status(500).json({
-            error,
-            message: 'Something went wrong on the server',
-        });
-    }
+router.put("/cohort/:cohortId", async (req, res) => {
+  try {
+    const updatedCohort = await Cohort.findByIdAndUpdate(
+      req.params.cohortId,
+      req.body,
+      { new: true }
+    );
+    res.status(202).json(updatedCohort);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.delete('/cohort/:cohortId', async (req, res) => {
-    try {
-        const cohortToDelete = await Cohort.findByIdAndDelete(
-            req.params.cohortId
-        );
-        res.status(202).json({
-            message: `${cohortToDelete.cohortName} was removed from the db`,
-        });
-    } catch (error) {
-        res.status(500).json({
-            error,
-            message: 'Something went wrong on the server',
-        });
-    }
+router.delete("/cohort/:cohortId", async (req, res) => {
+  try {
+    const cohortToDelete = await Cohort.findByIdAndDelete(req.params.cohortId);
+    res.status(202).json({
+      message: `${cohortToDelete.cohortName} was removed from the db`,
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
