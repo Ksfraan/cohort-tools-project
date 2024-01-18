6fd589c9430668df547b9e821e841e48b08d3268
Jesus Cidoncha <jesus.cidoncha.berlanga@gmail.com>
2024-01-18 15:26:14 +0100

    CORS updated for frontend connection

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5005"],
+    origin: ["http://localhost:5005", "http://localhost:5173"],
   })
 );
 app.use(express.json());
